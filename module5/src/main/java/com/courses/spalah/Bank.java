5a796d1ce5322768fd4e1270b08fbf80f1742a59
Artem Uskov <uskov.artem@gmail.com>
2016-07-19 23:39:44 +0300

    Merged 190716-2359-homePC

--- module5/src/main/java/com/courses/spalah/Bank.java
+++ module5/src/main/java/com/courses/spalah/Bank.java
@@ -24,29 +24,6 @@ public class Bank {
     public float paymentOfDeposit(int n){
         float payment = deposit1.getProfit(n) + deposit2.getProfit(n) + deposit3.getProfit(n);
         return payment;
-=======
- * Created by artem on 18.07.2016.
- */
-public class Bank {
-    String nameofbank;
-    Depozit Depozit1;
-    Depozit Depozit2;
-    Depozit Depozit3;
 
-    public Bank (String nameofbankofconstructor) {
-        nameofbank = nameofbankofconstructor;
-        Depozit Depozit1 = new Depozit(100, 15, "Ann");
-        Depozit Depozit2 = new Depozit(200, 13, "Tom");
-        Depozit Depozit3 = new Depozit(300, 10, "Can");
-    }
-
-
-    public static void main(String asrs[]){
-         int years = 3;
-         float viplata;
-
-
-
->>>>>>> 44f3ea71254363f4dd57cdcece8ae816ee3acbd0
     }
 }
