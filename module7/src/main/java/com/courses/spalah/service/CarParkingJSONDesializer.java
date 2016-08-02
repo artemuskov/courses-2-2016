e34eab4646efe76bf23159015f57a676c41f8f17
Artem Uskov <uskov.artem@gmail.com>
2016-08-02 08:03:07 +0300

    020816-0802-notebook

--- module7/src/main/java/com/courses/spalah/service/CarParkingJSONDesializer.java
+++ module7/src/main/java/com/courses/spalah/service/CarParkingJSONDesializer.java
@@ -14,7 +14,11 @@ public class CarParkingJSONDesializer implements CarParkingDeserializer {
     @Override
     public CarParking deserialize(String serializedParking){
         CarParking carParking = new CarParking();
+        //Car[] cars = new Car[getCarsNumber(serializedParking)];
         Car[] cars = new Car[getCarsNumber(serializedParking)];
+        System.out.println(getCarsNumber(serializedParking));
+        int[] quantityCars = new int[getCarsNumber(serializedParking)];
+
 
         serializedParking = serializedParking.replaceAll("\\{","");
         serializedParking = serializedParking.replaceAll("\\}","");
@@ -24,20 +28,84 @@ public class CarParkingJSONDesializer implements CarParkingDeserializer {
         serializedParking = serializedParking.replaceAll(",","");
         String[] parkingString = serializedParking.split("\"");
 
+        System.out.println("Исходные данные");
+        System.out.println(Arrays.toString(parkingString));
 
-        for (int i = 0; i < parkingString.length; i++){
+        System.out.println("Найденная парковка");
+        //Находим парковку и начало печати машин
+        int j = 0;
+        while (j < getCarsNumber(serializedParking)){
+        for (int i = 0; i < parkingString.length; i++) {
             if (parkingString[i].equals("address")) {
                 carParking.setAddress(parkingString[i + 2]);
             }
             if (parkingString[i].equals("parkingName")) {
                 carParking.setParkingName(parkingString[i + 2]);
             }
+            if (parkingString[i].equals("manufacturer")) {
+                quantityCars[j] = i;
+                j++;
+            }
         }
+        }
+        System.out.println(carParking.toString());
 
-        for (int j = 0; j < parkingString.length; j++) {
-            for (int i = 0; i < cars.length; i++) {
+        // Находим машины
+        System.out.println("length= " + getCarsNumber(serializedParking));
+        for (int n = 0; n < getCarsNumber(serializedParking); n++) {
+            for (int i = quantityCars[n]; i < quantityCars[n + 1]-1; i++) {
+                    System.out.println("i= " + i);
+                    System.out.println("n= " + n);
+                    System.out.println("q[n]=" + quantityCars[n]);
+                    System.out.println("q[n+1]=" + quantityCars[n+1]);
+                    System.out.println(parkingString[i]);
+                    System.out.println(parkingString[i + 2]);
+                if (parkingString[i].equals("manufacturer")) {
+                    cars[n].setManufacturer(parkingString[i + 2]);
+                    System.out.println("hello " + parkingString[i + 2]);
+                }
+                    if (parkingString[i] == ("manufacturer")) {
+                        //cars[n].setManufacturer(parkingString[i + 2]);
+                        System.out.println(parkingString[i + 2]);
+                    }
+                    if (parkingString[i].equals("modelName")) {
+                        cars[n].setModelName(parkingString[i + 2]);
+                    }
+                    if (parkingString[i].equals("vin")) {
+                        cars[n].setVin(parkingString[i + 2]);
+                    }
+                    if (parkingString[i].equals(("lengthMillimeters"))) {
+                        cars[n].setLengthMillimeters(Integer.parseInt(parkingString[i + 1].trim()));
+                    }
+                    if (parkingString[i].equals("heightMillimeters")) {
+                        cars[n].setHeightMillimeters(Integer.parseInt(parkingString[i + 1].trim()));
+                    }
+                }
+        }
+        System.out.println(cars.toString());
+
+
+
+
+       // car.setManufacturer("Mazda");
+        //String test = cars[0].getManufacturer();
+     //  System.out.println(car.getManufacturer());
+       // String[] cars = new String[5];
+      //  for (int i = 0; i < getCarsNumber(serializedParking); i++){
+
+     //   }
+
+
+
+
+      //  System.out.println(carParking.toString());
+      //  System.out.println("car.lenght = " + cars.length);
+
+      /*  for (int i = 0; i < cars.length; i++) {
+            for (int j = 0; j < parkingString.length; j++) {
                 if (parkingString[j].equals("manufacturer")) {
                     cars[i].setManufacturer(parkingString[j + 2]);
+                    System.out.println(i + j);
                 }
                 if (parkingString[j].equals("modelName")) {
                     cars[i].setModelName(parkingString[j + 2]);
@@ -53,12 +121,15 @@ public class CarParkingJSONDesializer implements CarParkingDeserializer {
                 }
             }
         }
+*/
+     //   System.out.println(cars.toString());
 
-        carParking.setCars(cars);
+      //  carParking.setCars(cars);
 
         //System.out.println("Машины" + Arrays.toString(cars));
-        //System.out.println("Текущая парковка - " + carParking.toString());
-        //System.out.print("Ожидаемая парковка - " + serializedParking);
+      //  System.out.println(serializedParking);
+       // System.out.println("Текущая парковка - " + carParking.toString());
+       // System.out.print("Ожидаемая парковка - " + serializedParking);
         return carParking;
     }
 
