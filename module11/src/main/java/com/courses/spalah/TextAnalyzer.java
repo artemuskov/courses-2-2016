2a9d517e7d985dac1d003b53975beed9677b27e7
Artem Uskov <uskov.artem@gmail.com>
2016-08-19 00:01:35 +0300

    190816-0000-homePC

--- module11/src/main/java/com/courses/spalah/TextAnalyzer.java
+++ module11/src/main/java/com/courses/spalah/TextAnalyzer.java
@@ -1,13 +1,32 @@
 package com.courses.spalah;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author Ievgen Tararaka
  */
 public class TextAnalyzer {
-    /**
-     * Ваш анализатор текста должен быть тут
-     */
-    public static void main(String[] args) {
+    private static String INPUT_FILE = "C:\\spalah\\courses-2-2016\\module11\\src\\main\\resources\\text_sample.txt";
+    private static String OUTPUT_FILE = "C:\\spalah\\courses-2-2016\\module11\\src\\main\\resources\\statistics.txt";
 
+
+
+    public static void main(String[] args) throws IOException {
+
+            FileReader fr = new FileReader("C:\\spalah\\courses-2-2016\\module11\\src\\main\\resources\\text_sample.txt");
+            Scanner sc = new Scanner(fr);
+            WordsCount.calculate();
+
+
+
+
+
+    }
+
+    public static String getInputFile() {
+        return INPUT_FILE;
     }
 }
---
+++ module11/src/main/java/com/courses/spalah/TokensCount.java
@@ -0,0 +1,11 @@
+package com.courses.spalah;
+
+/**
+ * Created by Artem Uskov on 18.08.2016.
+ */
+public class TokensCount {
+
+    public int tokensCount() {
+        return 0;
+    }
+}
