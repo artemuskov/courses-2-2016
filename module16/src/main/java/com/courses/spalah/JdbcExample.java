6ecbdd762c829c471a92e5c694dad16d2add88e0
Artem Uskov <uskov.artem@gmail.com>
2016-09-26 16:58:34 +0300

    260916-1700-workpc

--- module16/src/main/java/com/courses/spalah/JdbcExample.java
+++ module16/src/main/java/com/courses/spalah/JdbcExample.java
@@ -12,9 +12,9 @@ import java.sql.Statement;
 public class JdbcExample {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         registerDriver();
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "root"); // открытие соединения к базе
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "phant0m"); // открытие соединения к базе
         Statement statement = con.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT * FROM test.person");
+        ResultSet rs = statement.executeQuery("SELECT * FROM people.person");
         int columnCount = rs.getMetaData().getColumnCount();
         while (rs.next()) {
             for (int i = 1; i <= columnCount; i++) {
