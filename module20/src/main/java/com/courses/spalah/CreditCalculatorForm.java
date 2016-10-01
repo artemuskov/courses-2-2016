4cd4426aa26e7a876f0bab4a9d9854a8854bcc16
Artem Uskov <uskov.artem@gmail.com>
2016-10-01 17:41:22 +0300

    011016-1740-workpc

--- module20/src/main/java/com/courses/spalah/CreditCalculatorForm.java
+++ module20/src/main/java/com/courses/spalah/CreditCalculatorForm.java
@@ -18,7 +18,12 @@ public class CreditCalculatorForm extends JFrame{
     private Integer[] listOfTerms = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private JComboBox termOfCredit = new JComboBox(listOfTerms);
     private JTextField oneTimeCommisionSumm = new JTextField("Одноразовая комиссия, сумма");
-    private JTextField getEveryMounthCommisionSumm = new JTextField("Ежемесячная комиссия, сумма");
+    private JTextField everyMounthCommisionSumm = new JTextField("Ежемесячная комиссия, сумма");
+    private JButton calculateButton = new JButton("Рассчитать");
+    private JLabel prepaidSumm = new JLabel("Первоначальный взнос - ");
+    private JLabel creditSumm = new JLabel("Сумма кредита - ");
+    private JLabel oneTimeCommisionPay = new JLabel("Разовые комиссионые расходы - ");
+    private JLabel monthlyCommisionPay = new JLabel("Ежемесячные комиссионые расходы - ");
 
     public CreditCalculatorForm() {
         super("Credit Calculator");
@@ -27,10 +32,48 @@ public class CreditCalculatorForm extends JFrame{
 
     public void init() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(600, 400);
+        setSize(500, 600);
         setResizable(true);
         setVisible(true);
         panel.setLayout(null);
+        panel.add(totalBuySumm);
+        totalBuySumm.setBounds(20, 20, 200, 20);
+
+        panel.add(prepaidPercent);
+        prepaidPercent.setBounds(20, 60, 200, 20);
+
+        panel.add(oneTimeCommisionPercent);
+        oneTimeCommisionPercent.setBounds(20, 100, 200, 20);
+
+        panel.add(everyMounthCommisionPercent);
+        everyMounthCommisionPercent.setBounds(20, 140, 200, 20);
+
+        panel.add(annualRatePercent);
+        annualRatePercent.setBounds(240, 20, 200, 20);
+
+        panel.add(termOfCredit);
+        termOfCredit.setBounds(240, 60, 200, 20);
+
+        panel.add(oneTimeCommisionSumm);
+        oneTimeCommisionSumm.setBounds(240,100, 200, 20);
+
+        panel.add(everyMounthCommisionSumm);
+        everyMounthCommisionSumm.setBounds(240, 140, 200, 20);
+
+        panel.add(calculateButton);
+        calculateButton.setBounds(170, 170, 120, 30);
+
+        panel.add(prepaidSumm);
+        prepaidSumm.setBounds(20, 220, 300, 20);
+
+        panel.add(creditSumm);
+        creditSumm.setBounds(20, 240, 300, 20);
+
+        panel.add(oneTimeCommisionPay);
+        oneTimeCommisionPay.setBounds(20, 260, 300, 20);
+
+        panel.add(monthlyCommisionPay);
+        monthlyCommisionPay.setBounds(20, 280, 300, 20);
 
         add(panel);
 
