17e1d7f033e41c6d3b49a7500a12c4edef1fa413
Artem Uskov <uskov.artem@gmail.com>
2016-11-11 20:45:13 +0200

    111116-2100-notebook

--- module27/src/main/java/com/courses/spalah/Homework/Main.java
+++ module27/src/main/java/com/courses/spalah/Homework/Main.java
@@ -2,16 +2,21 @@ package com.courses.spalah.Homework;
 
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Created by Artem Uskov on 07.11.2016.
  */
 public class Main {
 
+    private static final String PERSISTENCE_UNIT = "com.courses.spalah.module27";
+
     public static void main(String[] args) {
 
-        Configuration cfg = new Configuration();
-        //cfg.configure("META-INF/persistence.xml");
-
-        //cfg.configure("hibernate.cfg.xml");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.close();
     }
 }
